11da2c993c5cf783162a92d22c18a2cd50d2dd05
valdidev <valdicode.fernando@gmail.com>
2022-10-12 16:07:55 +0200

    3 symbols in row, done

--- js/game.js
+++ js/game.js
@@ -1,19 +1,17 @@
 const xClass = 'symbol-x'
 const oClass = 'symbol-o'
 const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 const cells = Array.from(document.querySelectorAll('.cell'));
-
-let turn
-
+let turn;
 
 const startGame = () => {
     turn = true;
@@ -27,11 +25,16 @@ const startGame = () => {
 const handleClick = (e) => {
     let cell = e.target;
     let currentClass = turn ? xClass : oClass
-    placeMark(cell, currentClass);
-    swapTurn();
-}
 
-startGame();
+    placeMark(cell, currentClass);
+
+    if (checkWin(currentClass)) {
+        endGame()
+    } else {
+        swapTurn();
+    }
+
+}
 
 const placeMark = (cell, currentClass) => {
     cell.classList.add(currentClass);
@@ -48,3 +51,9 @@ const checkWin = (currentClass) => {
         })
     })
 }
+
+const endGame = () => {
+    console.log("end game")
+}
+
+startGame();
